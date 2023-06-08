fa4e8a129cbfae307b2c33ea06dc0e29cf1e6aba
vbth7777 <vbth7777@gmail.com>
2023-06-08 20:49:31 +0700

    08/06/2023

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const { exec } = require("child_process");
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const async = require('async');
+
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+const commandQueue = async.queue((task, callback) => task(callback));
 
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const greenColor = '\x1b[32m'; // ANSI escape sequence for green color
+const resetColor = '\x1b[0m'; // ANSI escape sequence to reset color
 
 app.post('/', (req, res) => {
     const url = req.body.url;
-    console.log(`Opening mpv (${url})...`)
-    exec(`mpv "${url}"`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
+    console.log(`${greenColor}Received request to open mpv ${resetColor}(${url})...`);
+
+    commandQueue.push(callback => {
+        console.log(`${greenColor}Executing mpv ${resetColor}(${url})...`);
+        exec(`mpv "${url}"`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`Error: ${error.message}`);
+                callback(error);
+            } else if (stderr) {
+                console.log(`Stderr: ${stderr}`);
+                callback(stderr);
+            } else {
+                console.log(`Stdout: ${stdout}`);
+                callback();
+            }
+        });
     });
+
+    res.sendStatus(200);
 });
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
