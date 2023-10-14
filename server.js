d403c3b7ef38f15486fceef0562344cdc64ba58e
vbth7777 <vbth7777@gmail.com>
2023-10-14 13:17:11 +0700

    14/10/2023

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 const async = require('async');
 const fs = require('fs');
 const axios = require('axios');
+const crypto = require('crypto');
 
 const app = express();
 const port = 9789;
@@ -16,6 +17,84 @@ const pathRunningUrls = path.join(__dirname, 'running-urls.txt');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+function convertToSHA1(str) {
+    const shasum = crypto.createHash('sha1');
+    shasum.update(str);
+    return shasum.digest('hex');
+}
+function getJSON(url, callback, headers) {
+    return axios.get(url, {
+        headers: headers
+    }).then(res => {
+        return callback(null, res.data);
+    }).catch(err => {
+        console.log('error: ' + url)
+        console.log(err)
+        return null;
+    });
+}
+async function getVideoUrl(url, accessToken) {
+    const getID = (url) => {
+        return url.match(/video\/([^\/]*)/)[1]
+    }
+    const getFileId = (url) => {
+        return url.match(/file\/.+\?/g)[0].replace(/file\/|\?/g, '')
+    }
+    const getExpire = (url) => {
+        return url.match("expires=.+&")[0].replace(/expires=|&/g, '');
+    }
+
+    const id = getID(url);
+    if (!id)
+        return null;
+    return await getJSON(`https://api.iwara.tv/video/${id}`, async (status, res) => {
+        if (status) {
+            console.log('Error: ', status);
+            return;
+        }
+        if (res.message && (res?.message?.trim()?.toLowerCase()?.includes('notfound') || res?.message?.trim()?.toLowerCase()?.includes('private'))) {
+            console.log(res.message + ' for ' + id)
+            return;
+        }
+        else if (res.message) {
+            console.log(res.message)
+            return;
+        }
+        if (res.embedUrl && !res.fileUrl) {
+            return res.embedUrl;
+        }
+        const fileUrl = res.fileUrl;
+        const fileId = getFileId(fileUrl)
+        if (!fileId || !fileUrl) {
+            console.log('Not found requirement');
+            return;
+        }
+        const vidResolution = [
+            'Source',
+            '540p',
+            '360p'
+        ]
+
+        // console.log((fileId + '_' + getExpire(fileUrl) + '_5nFp9kmbNnHdAFhaqMvt'))
+        return await getJSON(fileUrl, (status2, res2) => {
+            const json = res2;
+            // console.log(json)
+            let i = json.length - 1;
+            for (let j = 0; j < json.length; j++) {
+                if (vidResolution[0].toLowerCase().indexOf(json[j].name.toLowerCase()) != -1) {
+                    i = j;
+                    break;
+                }
+            }
+            const uri = 'https:' + json[i].src.download;
+            return uri
+        }, {
+            'x-version': convertToSHA1(fileId + '_' + getExpire(fileUrl) + '_5nFp9kmbNnHdAFhaqMvt'),
+            "Authorization": "Bearer " + accessToken
+        })
+    })
+
+}
 //WebSocket
 
 const wss = new WebSocketServer({ port: 9790 });
@@ -44,6 +123,7 @@ let urls = [];
 
 app.post('/', (req, res) => {
     const url = req.body.url;
+    const token = req.body.accessToken;
     const pageUrl = req.body.pageUrl == 'null' ? null : req.body.pageUrl;
     if (urls.includes(pageUrl || url)) {
         // console.log('Duplicate url: ', pageUrl || url)
@@ -71,10 +151,14 @@ app.post('/', (req, res) => {
 
     commandQueue.push(callback => {
         isMpvRunning = true;
-        console.log(`${greenColor}Executing mpv ${resetColor}(${content})...`);
         let countError = 0;
-        const execMpv = function() {
-            exec(`mpv "${url}" --fs --ytdl-format='bestvideo[height<=?2440]+bestaudio/best' --pause`, (error, stdout, stderr) => {
+        const execMpv = async function() {
+            let execUrl = url;
+            if (url.includes('iwara.tv')) {
+                execUrl = await getVideoUrl(url, token);
+            }
+            console.log(`${greenColor}Executing mpv ${resetColor}(${execUrl})...`);
+            exec(`mpv "${execUrl}" --fs --ytdl-format='bestvideo[height<=?2440]+bestaudio/best' --pause`, (error, stdout, stderr) => {
                 isMpvRunning = false;
                 if (error) {
                     console.log(`Error: ${error.message}`);
